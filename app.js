0df5e43471be6f578d16db51ea0675b93a5037b1
KateStearns <katestearns@mac.com>
2015-09-08 18:31:40 -0700

    CRUD features complete

--- app.js
+++ app.js
@@ -8,13 +8,67 @@ var app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
+app.set('views', __dirname + '/views');
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 mongoose.connect('mongodb://localhost/countries');
 mongoose.set('debug', true);
 
+var Country = new mongoose.Schema({
+  name: String,
+  flag: String,
+  capital: String,
+  population: Number
+});
+
+var countries = mongoose.model('countries', Country);
+
 app.get('/countries', function(req, res) {
-  res.send('I work!');
+  countries.find({}, function(err, data) {
+    countriesObj = data
+    res.render('index', {countries: countriesObj});
+  })
 })
+
+app.get('/countries/new', function(req, res) {
+  res.render('new');
+});
+
+app.get('/countries/:id/edit', function(req, res) {
+  res.render('edit');
+});
+
+app.get('/countries/:id/edit', function(req, res) {
+  countries.findByIdAndUpdate(req.params.id, req.body.country, function(err, country) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect('/countries', {countries: countriesObj});
+    }
+  })
+})
+
+app.post('/countries', function(req, res) {
+  console.log(req.body.country);
+  countries.create(req.body.country, function(err, country) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect('/countries');
+    }
+  })
+});
+
+app.delete('/countries/:id', function(req, res) {
+  countries.findByIdAndRemove(req.params.id, function(err, data) {
+    if(err) {
+      console.log(err)
+    } else {
+      res.redirect('/countries');
+    }
+  });
+});
 
 app.get('*', function(req, res) {
   res.send('This is not the page you are looking for. <a href="/countries">Go Back </a>');
